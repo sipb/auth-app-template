18766a0b0ccd190aaa6b6d855999ccc0ea3c0db2
Nate Foss <npfoss@gmail.com>
2020-04-16 17:16:28 -0400

    certs?

--- cgi-bin/backend.py
+++ cgi-bin/backend.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
 from cgi import FieldStorage
 from json import dumps, loads
+import os
 
 args = FieldStorage()
 
 error = 'Status: 400 Bad Request\n'
 content = 'Content-Type: application/json\n\n'
 
-if 'kerb' not in args:
-    output = {"error": "kerb was not specified"}
-    print(error + content)
-    print(dumps(output))
-else:
-    output = {"access-key": "hunter2"}
+if 'SSL_CLIENT_S_DN_Email' in os.environ:
+  print(os.environ['SSL_CLIENT_S_DN_Email'])
+  # add to database or something more fancy, probably create a password, do fancy checks and stuff
+elif:
+  # click this link -- whatever you're at now plus https and :444
+  # res.send("no certificate, please visit https://minecraft.scripts.mit.edu:444");
+  pass
+
+output = {}
+# if 'kerb' not in args:
+#     output = {"error": "kerb was not specified"}
+#     print(error + content)
+#     print(dumps(output))
+# else:
+#     output = {"access-key": "hunter2"}
 
 print(error + content)
 print(dumps(output))
