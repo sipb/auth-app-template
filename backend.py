e7441f0e5475ab5acdf260ec1d8578d0cac83397
Nate Foss <npfoss@gmail.com>
2020-04-26 12:20:44 -0400

    clean up comments

--- backend.py
+++ backend.py
@@ -8,24 +8,14 @@ args = FieldStorage()
 error = 'Status: 200 OK\n'
 content = 'Content-Type: text/html\n\n'
 
-output = {}
+output = None
 if 'SSL_CLIENT_S_DN_Email' in os.environ:
   output = { "kerb" : os.environ['SSL_CLIENT_S_DN_Email'] }
-  pass
+  # add to database or something more fancy, probably create a password, do fancy checks and stuff
 else:
   error = 'Status: 401 Unauthorized\n'
-#  content += os.environ['SSL_CLIENT_S_DN_Email']
-#  # add to database or something more fancy, probably create a password, do fancy checks and stuff
-#  # click this link -- whatever you're at now plus https and :444
-#  # res.send("no certificate, please visit https://minecraft.scripts.mit.edu:444");
-#  pass
-
-# if 'kerb' not in args:
-#     output = {"error": "kerb was not specified"}
-#     print(error + content)
-#     print(dumps(output))
-# else:
-#     output = {"access-key": "hunter2"}
+  # click this link -- whatever you're at now plus https and :444
+  # e.g. res.send("no certificate, please visit https://minecraft.scripts.mit.edu:444");
 
 print(error + content)
 print(dumps(output))
